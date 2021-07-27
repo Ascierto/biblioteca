ea6d2c93ea5b6ff97ccbcf7ae9082ec69df93be1
Giuliano Ascierto <giulianoascierto@gmail.com>
2021-07-27 23:09:17 +0200

    Aggiunta select dinamica in inserimento prestito

--- includes/Books.php
+++ includes/Books.php
@@ -1,7 +1,7 @@
 <?php
 namespace Biblos;
 
-include __DIR__ .'/Db.php';
+include_once __DIR__ .'/Db.php';
 
 class Book{
 
--- rent-insert.php
+++ rent-insert.php
@@ -7,14 +7,29 @@ if (!isset($_SESSION['is_admin'])) {
   header('Location: http://localhost:8888/biblioteca/?stato=errore&messages=Impossibile accedere');
 } 
 
+include __DIR__ . '/includes/Users.php';
+
+include __DIR__ . '/includes/Books.php';
+
 include __DIR__ .'/includes/header.php';
 
 include __DIR__ . '/includes/utils.php';
 
+
+
 if (isset($_GET['stato'])) {
   \Biblos\Utils\show_alert('inserimento', $_GET['stato']);
-  
-}
+  }
+
+$users= \Biblos\Users::showUsers();
+
+
+//se con 1 non va prova true
+$books= \Biblos\Book::showStatus(1);  
+
+
+
+
 ?>
 
 <h2>Libri in uscita</h2>
@@ -24,12 +39,21 @@ if (isset($_GET['stato'])) {
         <div class="col-12">
             <form method="POST" action="./includes/insert-rent.php">
               <div class="mb-3">
-                <label for="title" class="form-label">Titolo libro</label>
-                <input name="title" type="text" class="form-control" id="title" required>
+
+                <label class="form-label fw-bold my-2">Titolo libro</label>
+                <select name="title" class="form-select" id="title" required>
+                    <?php foreach($books as $book):?>
+                    <option id="title" value="<?php echo $book['title'] ?>"> <?php echo $book['title'] ?> </option>
+                    <?php endforeach; ?>
+                </select>
               </div>
               <div class="mb-3">
-                <label for="email" class="form-label">Email utente</label>
-                <input name="email" type="email" class="form-control" id="email" required>
+                <label class="form-label fw-bold my-2">Email Utente</label>
+                <select name="email" class="form-select" id="email" required>
+                    <?php foreach($users as $user):?>
+                    <option id="email" value="<?php echo $user['email'] ?>"> <?php echo $user['email'] ?> </option>
+                    <?php endforeach; ?>
+                </select>
               </div>
               <div class="mb-3 form-check">
                 <label for="date-end" class="form-label">Data fine prestito</label>
